6ffabec99fea2f3929b3273077957b4616bbc10a
fafifox <fafifox@protonmail.com>
2021-11-29 14:44:04 +0100

    i18n base

---
+++ template/js/index.js
@@ -0,0 +1,22 @@
+import Translator from "./translator.js";
+
+var translator = new Translator({
+  defaultLanguage: 'en',
+  detectLanguage: true,
+  selector: '[data-i18n]',
+  debug: false,
+  registerGlobally: '__',
+  persist: true,
+  persistKey: 'preferred_language',
+  filesLocation: '/i18n',
+});
+
+translator.fetch(["en", "fr", "es", "ru", "zh", "ar", "jp", "pt", "et"]).then(() => {
+  translator.translatePageTo(translator.defaultLanguage);
+});
+
+document.getElementById("lang-switcher").addEventListener("click", function(evt) {
+  if (evt.target.tagName === "A") {
+    translator.translatePageTo(evt.target.textContent);
+  }
+});
